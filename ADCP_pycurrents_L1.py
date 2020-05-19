0a864724b1d88a720df38d6957bf08c963121be2
Hana Hourston <50595197+hhourston@users.noreply.github.com>
2020-05-19 13:02:58 -0700

    Update ADCP_pycurrents_L1.py

--- ADCP_pycurrents_L1.py
+++ ADCP_pycurrents_L1.py
@@ -7,7 +7,7 @@ for L1 processing raw ADCP data.
 
 Contributions from: Di Wan, Eric Firing
 
-User input (3 places) needed after correct_true_north() function
+User input (4 places) needed after ) function
 """
 
 import os
@@ -63,11 +63,25 @@ def correct_true_north(mag_decl, measured_east, measured_north):  # change angle
 # wd = 'your/wd/here'
 # os.chdir(wd)
 
-# Specify raw ADCP file to create nc file from, along with associated csv metadata file
+# Specify raw ADCP file to create nc file from, along with associated csv metadata file and
+# average magnetic declination over the timeseries
+
+# 1) raw .000 file
+# inFile = 'your/path/here'
+# 2) csv metadata file
+# file_meta = 'your/path/here'
+# 3) average magnetic declination over the time series
+# magnetic_variation = ''
+
 # 1) raw .000 file
 raw_file = "./sample_data/a1_20050503_20050504_0221m.000"
 # 2) csv metadata file
 raw_file_meta = "./sample_data/a1_20050503_20050504_0221m_meta_L1.csv"
+
+# If your raw file came from a NarrowBand instrument, you must also use the start_year optional kwarg (int type)
+
+# If your raw file has time values out of range, you must also use the time_file optionalkwarg
+# This should be a csv file with time entries spanning the range of deployment
 
 
 def nc_create_L1(inFile, file_meta, start_year=None, time_file=None):
