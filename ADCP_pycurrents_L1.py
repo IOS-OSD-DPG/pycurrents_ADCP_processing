80259938aa3e4d3e75414bdf64bc248f67b9927d
Hana Hourston <50595197+hhourston@users.noreply.github.com>
2020-05-19 13:02:58 -0700

    Update ADCP_pycurrents_L1.py

--- ADCP_pycurrents_L1.py
+++ ADCP_pycurrents_L1.py
@@ -11,6 +11,8 @@ User input (4 places) needed after ) function
 """
 
 import os
+import glob as glob
+import fnmatch
 import csv
 import numpy as np
 import xarray as xr
@@ -58,7 +60,7 @@ def correct_true_north(mag_decl, measured_east, measured_north):  # change angle
 
 # User input
 
-# 1) wd = 'your/wd/here'
+# wd = 'your/wd/here'
 # os.chdir(wd)
 
 # Specify raw ADCP file to create nc file from, along with associated csv metadata file and
@@ -71,16 +73,15 @@ def correct_true_north(mag_decl, measured_east, measured_north):  # change angle
 # 3) average magnetic declination over the time series
 # magnetic_variation = ''
 
-# 2) raw .000 file
+# 1) raw .000 file
 raw_file = "./sample_data/a1_20050503_20050504_0221m.000"
-# 3) csv metadata file
+# 2) csv metadata file
 raw_file_meta = "./sample_data/a1_20050503_20050504_0221m_meta_L1.csv"
 
 # If your raw file came from a NarrowBand instrument, you must also use the start_year optional kwarg (int type)
 
-# If your raw file has time values out of range, you must also use the time_file optional kwarg
-# Use it to read in a csv file containing time entries spanning the range of deployment and following the
-# instrument sampling interval
+# If your raw file has time values out of range, you must also use the time_file optionalkwarg
+# This should be a csv file with time entries spanning the range of deployment
 
 
 def nc_create_L1(inFile, file_meta, start_year=None, time_file=None):
