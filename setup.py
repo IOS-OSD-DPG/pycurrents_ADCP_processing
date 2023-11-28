e42394da1d73a821640218fe868d864e137c45a4
hhourston <50595197+hhourston@users.noreply.github.com>
2023-11-28 13:08:10 -0800

    update setup.py file package version to 0.0.2, add Maxim as author, add '*yml' files as package_data, update url

--- setup.py
+++ setup.py
@@ -5,16 +5,17 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pycurrents_ADCP_processing",
-    version="0.0.1",
-    author="Hana Hourston, Di Wan, Lu Guan",
+    version="0.0.2",
+    author="Hana Hourston, Di Wan, Lu Guan, Maxim Krassovski",
     author_email="hana.hourston@dfo-mpo.gc.ca",
-    description="A Python package for processing Acoustic Doppler Current Profiler (ADCP) data and exporting it in netCDF file format.",
+    description="A Python package for processing Acoustic Doppler Current Profiler (ADCP) data.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/hhourston/pycurrents_ADCP_processing",
-    install_requires=['numpy', 'xarray', 'pandas', 'datetime', 'gsw', 'netCDF4', 'scipy', 'matplotlib', 'shapely'],
+    url="https://github.com/IOS-OSD-DPG/pycurrents_ADCP_processing",
+    install_requires=['numpy', 'xarray', 'pandas', 'datetime', 'gsw', 'netCDF4', 'scipy', 'matplotlib', 'shapely',
+                      'ruamel.yaml'],
     packages=setuptools.find_packages(),
-    package_data={'pycurrents_ADCP_processing': ['*.geojson']},
+    package_data={'pycurrents_ADCP_processing': ['*.geojson', '*.yml']},
     classifiers=["Programming Language :: Python :: 3"],
     python_requires='~=3.7',
 )
