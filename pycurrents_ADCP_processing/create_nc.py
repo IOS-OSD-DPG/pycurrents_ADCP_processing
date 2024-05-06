6796f5dd5dbdd3227a04ad8a406398c1688dad96
hhourston <50595197+hhourston@users.noreply.github.com>
2024-05-06 15:32:58 -0700

    update example script

--- pycurrents_ADCP_processing/create_nc.py
+++ pycurrents_ADCP_processing/create_nc.py
@@ -1,32 +1,35 @@
 """
 Sample usage of:
-    - ADCP_processing_L1.py
-    - ADCP_processing_L0.py
-    - add_var2nc.py
+    - ADCP_processing_L0_L1.py
     - ADCP_IOS_Header_file.py
 
 Outputs:
-    - L0 netCDF ADCP file without geographic_area variable
     - L0 netCDF ADCP file with geographic_area variable
-    - L1 netCDF ADCP file without geographic_area variable
     - L1 netCDF ADCP file with geographic_area variable
     - IOS .adcp header file (produced from L1 netCDF ADCP file with geographic_area variable)
+    - Full suite of plots for output L1 files
 """
 
-from pycurrents_ADCP_processing import ADCP_processing_L0, ADCP_processing_L1, ADCP_IOS_Header_file
-from deprecated import add_var2nc
+from pycurrents_ADCP_processing import ADCP_processing_L0_L1, ADCP_IOS_Header_file
+from pycurrents_ADCP_processing import plot_westcoast_nc_LX
+# from deprecated import add_var2nc
 
 # Define raw ADCP file and associated metadata file
 f = './sample_data/a1_20050503_20050504_0221m.000'
-meta = './sample_data/a1_20050503_20050504_0221m_meta_L1.csv'
+meta = './sample_data/a1_20050503_20050504_0221m_metadata.csv'
 dest_dir = 'dest_dir'
 
 # Perform L0 processing on the raw data and export as a netCDF file
-ncname_L0 = ADCP_processing_L0.nc_create_L0(f_adcp=f, f_meta=meta, dest_dir=dest_dir)
+# ncname_L0 = ADCP_processing_L0.nc_create_L0(f_adcp=f, f_meta=meta, dest_dir=dest_dir)
+ncname_L0 = ADCP_processing_L0_L1.nc_create_L0_L1(in_file=f, file_meta=meta, dest_dir=dest_dir, level=0)
+
 
 # Perform L1 processing on the raw data and export as a netCDF file
-ncname_L1 = ADCP_processing_L1.nc_create_L1(in_file=f, file_meta=meta, dest_dir=dest_dir)
+ncname_L1 = ADCP_processing_L0_L1.nc_create_L0_L1(in_file=f, file_meta=meta, dest_dir=dest_dir, level=1)
 
 # Generate a header (.adcp) file from the L1 netCDF file that has the geographic area variable
 header_name = ADCP_IOS_Header_file.main_header(ncname_L1, dest_dir)
 
+plot_list = plot_westcoast_nc_LX.create_westcoast_plots(
+    ncname_L1, dest_dir, do_all_plots=True
+)
