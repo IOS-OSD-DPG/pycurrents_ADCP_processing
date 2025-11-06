70ba2f9fe1ba51ed95297d8ad05b3f2f28cf2d37
IOS-OSD-DATA <99845558+IOS-OSD-DATA@users.noreply.github.com>
2025-11-06 09:31:35 -0800

    Add files via upload

--- pycurrents_ADCP_processing/create_nc.py
+++ pycurrents_ADCP_processing/create_nc.py
@@ -12,12 +12,16 @@ Outputs:
 
 from pycurrents_ADCP_processing import ADCP_processing_L0_L1, ADCP_IOS_Header_file
 from pycurrents_ADCP_processing import plot_westcoast_nc_LX
+from pycurrents_ADCP_processing import ADCP_IOS_Header_file
+import os
 # from deprecated import add_var2nc
 
 # Define raw ADCP file and associated metadata file
-f = './sample_data/a1_20050503_20050504_0221m.000'
-meta = './sample_data/a1_20050503_20050504_0221m_metadata.csv'
-dest_dir = 'dest_dir'
+#current_dir = os.getcwd()
+os.getcwd() # check current working directory
+f = './a1_20120616_20140512_0490m.000'
+meta = './a1_20120616_20140512_0490m_metadata.csv'
+dest_dir = 'processed'
 
 # Perform L0 processing on the raw data and export as a netCDF file
 # ncname_L0 = ADCP_processing_L0.nc_create_L0(f_adcp=f, f_meta=meta, dest_dir=dest_dir)
@@ -30,5 +34,7 @@ ncnames_L1 = ADCP_processing_L0_L1.nc_create_L0_L1(in_file=f, file_meta=meta, de
 # Generate a header (.adcp) file from the L1 netCDF file that has the geographic area variable
 for n in ncnames_L1:
     header_name = ADCP_IOS_Header_file.main_header(n, dest_dir)
-
     plot_list = plot_westcoast_nc_LX.create_westcoast_plots(n, dest_dir, do_all_plots=True)
+
+
+
